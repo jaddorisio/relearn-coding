e77519169b0e18bbbca36a09bfa6e81ac44a8aa1
Jonpaul Addorisio <jaddorisio97@gmail.com>
2026-03-21 17:18:39 -0400

    ex2 Intial

---
+++ python/Learn-Python-The-Hard-Way3/exercise2/ex2.py
@@ -0,0 +1,8 @@
+# A comment, this is so you can read your program later 
+# Anything after # is ignored by python
+
+print("I could have code like this.")# And the comment after is ignored
+
+# you can alose use a comment to "disable" or comment out code:
+# print("This wont run.")
+print("This will run.")
