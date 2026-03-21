c0d1a57f238b4951c0d4afee6d74e526abef543f
Jonpaul Addorisio <jaddorisio97@gmail.com>
2026-03-21 17:10:32 -0400

    excersise1

---
+++ python/Learn-Python-The-Hard-Way3/exercise1/ex1.py
@@ -0,0 +1,7 @@
+print("Hello world")
+print("Hello again")
+print("I like typing this")
+print("This is fun")
+print('Yay! Printing.')
+print("I'd mcuh rather you 'not'")
+print('I "said" do not touch this')
