a48f60e97a039c4d151c5b9389d75a00350b6217
Jonpaul Addorisio <jaddorisio97@gmail.com>
2026-03-22 21:06:01 -0400

    finish excersise 3

--- python/Learn-Python-The-Hard-Way3/ex3.py
+++ python/Learn-Python-The-Hard-Way3/ex3.py
@@ -1,10 +1,16 @@
+
+#Write statement to terminal
 print("I will now count my chickens:")
 
-print("Hens", 25 + 30 / 6)
-print("Roosters", 100 - 25 * 3 % 4)
+#Prin hens plus pemdas 
+print("Hens", 25.0 + 30.0 / 6.0)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
 
+
+# Write coutting eggs
 print("Now I will count the eggs:")
-print(3 + 2 + 1 - 5 + 4 % 2 - 1)
+# Write value of the equation 
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0  % 2 - 1 / 4+ 6)
 print("is it true that 3 + 2 < 5 - 7")
 
 print(3 + 2 < 5 - 7)
@@ -12,4 +18,9 @@ print(3 + 2 < 5 - 7)
 print("Now What is ")
 
 print("Oh, that's why it's False.")
-print()
+print("How about some more.")
+
+print("Is it greater or equal?", 5 >= -2)
+
+print("Is it less or equal?", 5 <= -2)
+
