9409e676f2d74d0ae66f5bd9069b61ade1b21ac2
Jonpaul Addorisio <jonpaul.addorisio@mclane.com>
2026-04-14 14:17:15 -0400

    Add Exercise 10: Escape sequences

---
+++ python/Learn-Python-The-Hard-Way3/ex10.py
@@ -0,0 +1,18 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat. "
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+
+The boss yelled, "Don't press that button!"
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
